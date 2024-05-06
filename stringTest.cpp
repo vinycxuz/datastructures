d89d70a057bc56b5c2581fb1a972663a62e5bec4
vinycxuz <vinyby@gmail.com>
2024-05-06 10:38:30 -0300

    feat: using strings

---
+++ stringTest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+using std::string;
+
+int main () {
+  string s = "know";
+  string t = "ledge";
+  string u = s + t;
+
+   string test = "smart";
+   int ind = test.find("art");
+   int ind2 = test.size();
+}
